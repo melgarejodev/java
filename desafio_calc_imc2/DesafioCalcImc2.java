b5af872e7743ee4e42d22cfd3644525cc72a2201
Melgarejo.dev <melgarejo.dev@gmail.com>
2023-05-17 01:45:16 -0300

    Desafio concluído

---
+++ desafio_calc_imc2/DesafioCalcImc2.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class DesafioCalcImc2 {
+
+  public static void main(String[] args) {
+
+    String executa = "X";
+
+    while (!executa.equalsIgnoreCase("n")) {
+      System.out.println("---");
+      System.out.println("Deseja calcular o IMC ? (\"S\"-sim, \"N\"-nao)");
+      System.out.println("---");
+
+      executa = inputString();
+
+      if (executa.equalsIgnoreCase("s")) {
+
+        run();
+        executa = "X";
+
+      }
+
+    }
+
+  }
+
+  public static String inputString() {
+
+    Scanner input = new Scanner(System.in);
+    return input.nextLine();
+
+  }
+
+  public static double inputDouble() {
+
+    Scanner input = new Scanner(System.in);
+    return input.nextDouble();
+
+  }
+
+  public static void run() {
+
+    double peso = getPeso();
+    double altura = getAltura();
+
+    double imc = getImc(peso, altura);
+
+    System.out.println("Calculo do IMC: " + imc + " (Peso: " + peso + " kg / Altura: " + (altura / 100) + " m)");
+    System.out.println("Classificacao: " + getClassificacao(imc));
+
+  }
+
+  public static double getAltura() {
+
+    System.out.println("Informe a Altura em CM: ");
+    double h = inputDouble();
+    return h;
+
+  }
+
+  public static double getPeso() {
+
+    System.out.println("Informe o Peso em KG: ");
+    double p = inputDouble();
+    return p;
+
+  }
+
+  public static double getImc(double peso, double altura) {
+
+    // Altura em cm, devera ser convertida para M antes de ^2
+    return peso / Math.pow((altura / 100), 2);
+
+  }
+
+  public static String getClassificacao(double imc) {
+
+    final double pesoImc[] = {18.5, 24.9, 29.9, 34.9, 39.9};
+    final String classeImc[] = {"Magreza", "Normal", "Sobrepeso", "Obesidade", "Obesidade severa", "Obesidade morbida"};
+
+    int i = 0;
+    for(double lim: pesoImc) {
+      if(lim <= imc) {
+        i++;
+      }
+    }
+
+    return classeImc[i];
+
+  }
+
+}
---
+++ desafio_calc_imc2/readme.md
@@ -0,0 +1,29 @@
+# Desafio 2 - Calculo do IMC
+Antes de iniciar os cálculos, o usuário deverá responder ao programa:
+
+---
+**Deseja calcular o IMC ? ("S"-sim, "N"-nao)**
+---
+
+Se o usuario digitar "N" ou "n", o programa se encerra.
+Se o usuario digitar "S" ou "s", os dados do IMC deverao ser coletados para calculo, a exemplo do que foi feito anteriormente.
+Se o usuario digitar algum valor difererente de "SsNn", o programa deverá repetir a pergunta.
+
+O cálculo do imc deverá ser realizado utilizando a fórmula abaixo.
+
+**Fórmula do IMC:**
+
+```java
+
+imc = peso / altura ^ 2
+
+```
+
+**Tabela de classificacao:**
+
+1. até 18,5 - Magreza
+2. até 24,9 - Normal
+3. até 29,9 - Sobrepeso
+4. até 34,9 - Obesidade
+5. até 39,9 - Obesidade severa
+6. maior ou igual a 40 - Obesidade morbida
